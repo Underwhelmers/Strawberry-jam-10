c9134deb58d152c0d0270b6cd54e39a0d02cf982
Hennico <104310476+Underwhelmers@users.noreply.github.com>
2026-02-03 23:55:50 -0300

    connecting main to bar

--- encounter/input-handler.gd
+++ encounter/input-handler.gd
@@ -2,6 +2,11 @@ extends Node
 
 @onready var action_bar : EncounterProgressBar = $"../UI/ActionBar"
 
+func _ready():
+	action_bar.threshold_reached.connect(threshold_reached)
+
 func _process(delta):
 	action_bar.add_value(delta)
 	
+func threshold_reached(val: int):
+	print_debug("We reached threshold: ", val)
